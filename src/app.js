e594000a3418f5f641ee166b6d26b0b9e254a3c7
radoks83 <radkevych.oksana@gmail.com>
2022-06-08 17:34:29 +0200

    Updated current date

--- src/app.js
+++ src/app.js
@@ -1,5 +1,28 @@
-let apiKey = "149e1223e69e53cd644a15607bc75a82";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+// Current date
+function formatDate(timestemp) {
+  let date = new Date(timestemp);
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hour}:${minutes}`;
+}
+
+// Current Temperature
 function showTemp(response) {
   let temp = document.getElementById("temp");
   temp.innerHTML = Math.round(response.data.main.temp);
@@ -11,5 +34,12 @@ function showTemp(response) {
   humidity.innerHTML = response.data.main.humidity;
   let windSpeed = document.getElementById("windSpeed");
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  let date = document.getElementById("date");
+  date.innerHTML = formatDate(response.data.dt * 1000);
 }
+
+// Api Request
+let cityName = "New York";
+let apiKey = "149e1223e69e53cd644a15607bc75a82";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(showTemp);
