1c7e21d422210ab947226793a0b702d1438136c3
radoks83 <radkevych.oksana@gmail.com>
2022-06-08 16:56:44 +0200

    Showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-// alert("Hello World");
+let apiKey = "149e1223e69e53cd644a15607bc75a82";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+function showTemp(response) {
+  let temp = document.getElementById("temp");
+  temp.innerHTML = Math.round(response.data.main.temp);
+  let city = document.getElementById("city");
+  city.innerHTML = response.data.name;
+  let description = document.getElementById("description");
+  description.innerHTML = response.data.weather[0].description;
+  let humidity = document.getElementById("humidity");
+  humidity.innerHTML = response.data.main.humidity;
+  let windSpeed = document.getElementById("windSpeed");
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+}
+axios.get(apiUrl).then(showTemp);
